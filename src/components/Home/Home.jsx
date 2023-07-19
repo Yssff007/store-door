d5e1fab75dfb92f3b2ab02064a7bf2beb8cfd7cd
Yssff007 <youssefhussien116@gmail.com>
2023-07-19 12:39:39 +0200

    new repo

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import homeImg from './assets/Images/hero-img.png';
+import './assets/style/Home.css';
+export default function Home() {
+  const year = new Date().getFullYear
+  return (
+    <>
+    <section className='home_section'>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-5">
+            <div className="home_content">
+              <p className='home_subtitle'>Trending product in {year}</p>
+              <h2>Make Your Interior More Minimalistic & Modern </h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dicta,
+              aliquam voluptas repellendus possimus itaque rem ipsum nulla et tempore.</p>
+              <button type="button" class="btn btn-success">SHOP NOW</button>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="home_image">
+              <img src={homeImg} alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div className="container p-5">
+      <div className="row g-5 mb-5">
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-md-4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Link } from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light by-5">
+  <div className="container-fluid">
+    <Link class="navbar-brand" to="">
+      <i className='fas fa-shop px-2'></i>
+      Store Door
+    </Link>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mx-auto mb-lg-0 px-auto  ">
+        <li className="nav-item  ">
+          <Link className="nav-Link text-dark active text-decoration-none mx-1" aria-current="page" to=''>Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-Link text-dark text-decoration-none mx-1" to='About'>About</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-Link text-dark text-decoration-none mx-1" to='Services'>Services</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-Link text-dark text-decoration-none mx-1" to='Contacts'>Contacts</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-Link text-dark text-decoration-none mx-1" to='Categories'>Categories</Link>
+        </li>
+        {/* <li className="nav-item dropdown">
+          <Link className="nav-link dropdown-toggle ms-1 p-0" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </Link>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><Link className="dropdown-item" to="#">Action</Link></li>
+            <li><Link className="dropdown-item" to="#">Another action</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><Link className="dropdown-item" to="#">Something else here</Link></li>
+          </ul>
+        </li> */}
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
